1f6088a3f24f467091cedfe304176066b4153e21
q291793758 <291793758@qq.com>
2016-11-09 12:28:40 +0800

    user ,role, info模块 查询实现

--- src/cn/itcast/nsfw/info/action/InfoAction.java
+++ src/cn/itcast/nsfw/info/action/InfoAction.java
@@ -1,7 +1,7 @@
 package cn.itcast.nsfw.info.action;
 
 import cn.itcast.core.action.BaseAction;
-import cn.itcast.core.utils.QueryHelper;
+import cn.itcast.core.util.QueryHelper;
 import cn.itcast.nsfw.info.entity.Info;
 import cn.itcast.nsfw.info.service.InfoService;
 import com.opensymphony.xwork2.ActionContext;
@@ -10,6 +10,7 @@ import org.apache.struts2.ServletActionContext;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.net.URLDecoder;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
@@ -20,20 +21,23 @@ public class InfoAction extends BaseAction {
     private List<Info> infoList;
     private Info info;
 
+
     //列表页面
-    public String listUI() throws Exception{
+    public String listUI() throws Exception {
         try {
             //加载分类集合
-            ActionContext.getContext().getContextMap().put("infoTypeMap", Info.INFO_TYPE_MAP);
+                ActionContext.getContext().getContextMap().put("infoTypeMap", Info.INFO_TYPE_MAP);
             QueryHelper qh = new QueryHelper(Info.class, "i");
-            if (info != null) {
-                if (StringUtils.isNotBlank(info.getTitle())) {
-                    qh.addQueryCondition("i.title like ?","%"+info.getTitle()+"%");
+
+            if (searchString != null) {
+                searchString = URLDecoder.decode(searchString, "utf-8");
+                if (StringUtils.isNotBlank(searchString)) {
+                    qh.addQueryCondition("i.title like ?", "%" + searchString + "%");
                 }
-                qh.addQueryCondition("i.state = ?","1");
+                qh.addQueryCondition("i.state = ?", "1");
+                qh.addOrderByCondition("i.createTime", qh.ORDER_BY_DESC);
             }
-            qh.addOrderByCondition("i.createTime",qh.ORDER_BY_DESC);
-            infoList=infoService.findObjects(qh);
+            infoList = infoService.findObjects(qh);
         } catch (Exception e) {
             throw new Exception(e.getMessage());
         }
@@ -41,53 +45,59 @@ public class InfoAction extends BaseAction {
     }
 
     //跳转到新增页面
-    public String addUI(){
+    public String addUI() {
         //加载分类集合
         ActionContext.getContext().getContextMap().put("infoTypeMap", Info.INFO_TYPE_MAP);
         //在进入编辑页面的时候传入当前创建时间
-        info=new Info();
+        info = new Info();
         info.setCreateTime(new Timestamp(new Date().getTime()));
         return "addUI";
     }
+
     //保存新增
-    public String add(){
-        if(info!=null ){
+    public String add() {
+        if (info != null) {
             infoService.save(info);
         }
         return "list";
     }
+
     //跳转到编辑界面
-    public String editUI(){
+    public String editUI() {
         //加载分类集合
         ActionContext.getContext().getContextMap().put("infoTypeMap", Info.INFO_TYPE_MAP);
-        if(info!=null && info.getInfoId()!=null){
-            info=infoService.findObjectById(info.getInfoId());
+        if (info != null && info.getInfoId() != null) {
+            info = infoService.findObjectById(info.getInfoId());
         }
         return "editUI";
     }
+
     //保存编辑
-    public String edit(){
+    public String edit() {
         infoService.update(info);
         return "list";
     }
+
     //删除
-    public String delete(){
-        if(info!=null && info.getInfoId()!=null){
+    public String delete() {
+        if (info != null && info.getInfoId() != null) {
             infoService.delete(info.getInfoId());
         }
         return "list";
     }
+
     //批量删除
-    public String deleteSelected(){
-        if(selectedRow!=null){
-            for(String id:selectedRow){
+    public String deleteSelected() {
+        if (selectedRow != null) {
+            for (String id : selectedRow) {
                 infoService.delete(id);
             }
         }
         return "list";
     }
+
     //信息发布
-    public void publicInfo(){
+    public void publicInfo() {
         try {
             if (info != null) {
                 //1.更新信息状态
@@ -133,4 +143,5 @@ public class InfoAction extends BaseAction {
     public void setInfo(Info info) {
         this.info = info;
     }
+
 }
